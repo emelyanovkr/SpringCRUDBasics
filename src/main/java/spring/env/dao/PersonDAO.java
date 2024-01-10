01cf9fb8377700587840484b2950e54415b42a46
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-01-10 22:07:06 +0300

    READ operation added

---
+++ src/main/java/spring/env/dao/PersonDAO.java
@@ -0,0 +1,35 @@
+package spring.env.dao;
+
+import org.springframework.stereotype.Component;
+import spring.env.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO
+{
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Kirill"));
+        people.add(new Person(++PEOPLE_COUNT, "Petr"));
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+    }
+
+    public List<Person> index()
+    {
+        return people;
+    }
+
+    public Person show(int id)
+    {
+        return people.stream()
+                .filter(person -> person.getId() == id)
+                .findAny()
+                .orElse(null);
+    }
+}
