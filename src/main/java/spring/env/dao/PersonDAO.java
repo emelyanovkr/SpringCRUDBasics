3e474df5f706a3d1b4feab546231b6c1e6bc1669
Kirill Emelyanov <k.emelyanov070@gmail.com>
2024-01-11 16:54:36 +0300

    CREATE operation added

--- src/main/java/spring/env/dao/PersonDAO.java
+++ src/main/java/spring/env/dao/PersonDAO.java
@@ -32,4 +32,10 @@ public class PersonDAO
                 .findAny()
                 .orElse(null);
     }
+
+    public void save(Person person)
+    {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
 }
